2fe2ced3bbbdf1914c48ab4f7904551965309818
David Salek <salekd@gmail.com>
2018-03-05 23:05:08 +0000

    improved dropdown menu

--- dashboard.py
+++ dashboard.py
@@ -26,23 +26,42 @@ app.layout = html.Div([
     dcc.Dropdown(
         id='dropdown_device',
         options=[{'label': x, 'value': x} for x in devices],
-        value=devices[0]
+        value=devices,
+        multi=True
+    ),
+    dcc.Dropdown(
+        id='dropdown_variable',
+        options=[
+            {'label': 'moisture', 'value': 'moisture'},
+            {'label': 'temperature', 'value': 'temperature'},
+            {'label': 'conductivity', 'value': 'conductivity'},
+            {'label': 'light', 'value': 'light'},
+        ],
+        value='moisture'
     ),
     dcc.Graph(id='graph_temperature')
-], style={'width': '500'})
+], style={'width': '800'})
 
 
-@app.callback(Output('graph_temperature', 'figure'), [Input('dropdown_device', 'value')])
-def update_graph_temperature(device):
-    query = "SELECT * FROM measurements WHERE device='{}'".format(device)
+@app.callback(Output('graph_temperature', 'figure'),
+        [Input('dropdown_device', 'value'), Input('dropdown_variable', 'value')])
+def update_graph_temperature(device_list, variable):
+    #query = "SELECT * FROM measurements WHERE device IN ({})".format(
+    #        ", ".join(["'{}'".format(device) for device in device_list]))
+    query = "SELECT * FROM measurements"
     df = pd.read_sql_query(query, con=db)
-    print(df)
     return {
         'data': [{
-            'x': df['time'],
-            'y': df['temperature']
-        }],
-        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+            'x': df[df['device']==device]['time'],
+            'y': df[df['device']==device][variable],
+            'mode': 'lines+markers',
+            'name': device
+        } for device in device_list],
+        'layout': {
+            'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30},
+            'xaxis': {'title': 'time'},
+            'yaxis': {'title': variable},
+        }
     }
 
 
