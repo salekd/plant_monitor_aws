43d6c3577315eed11514de36df188c20fe3439a3
David Salek <salekd@gmail.com>
2018-03-05 23:05:26 +0000

    dashboard

--- ec2_start.sh
+++ ec2_start.sh
@@ -7,3 +7,5 @@ sudo service docker start
 sudo nohup docker run -p 80:8080 -e SWAGGER_JSON=/spec/swagger.json -e VALIDATOR_URL=null -v /home/ec2-user/plant_monitor_aws/:/spec swaggerapi/swagger-ui > /home/ec2-user/swagger-ui.out 2>&1 &
 
 sudo nohup jupyterhub > /home/ec2-user/jupyterhub.out 2>&1 &
+
+cd /home/ec2-user/plant_monitor_aws && sudo nohup python dashboard.py > /home/ec2-user/dashboard.out 2>&1 &
