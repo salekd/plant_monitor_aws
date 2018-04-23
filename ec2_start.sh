e7e66887c6992d7737bd9412947ee3bb89753969
David Salek <salekd@gmail.com>
2018-04-23 23:11:06 +0000

    no need to start swagger through docker

--- ec2_start.sh
+++ ec2_start.sh
@@ -3,8 +3,8 @@ sudo service postgresql start
 
 cd /home/ec2-user/plant_monitor_aws && sudo nohup python flask_app.py > /home/ec2-user/flask_app.out 2>&1 &
 
-sudo service docker start
-sudo nohup docker run -p 80:8080 -e SWAGGER_JSON=/spec/swagger.json -e VALIDATOR_URL=null -v /home/ec2-user/plant_monitor_aws/:/spec swaggerapi/swagger-ui > /home/ec2-user/swagger-ui.out 2>&1 &
+#sudo service docker start
+#sudo nohup docker run -p 80:8080 -e SWAGGER_JSON=/spec/swagger.json -e VALIDATOR_URL=null -v /home/ec2-user/plant_monitor_aws/:/spec swaggerapi/swagger-ui > /home/ec2-user/swagger-ui.out 2>&1 &
 
 sudo nohup jupyterhub > /home/ec2-user/jupyterhub.out 2>&1 &
 
